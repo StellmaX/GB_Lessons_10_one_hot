769cffb70419545eb5a8a6230873bae35febfee5
StellmaX <anb-s@bk.ru>
2024-07-16 23:42:23 +0700

    Навели лоск

--- one_hot.py
+++ one_hot.py
@@ -3,11 +3,10 @@ lst = ['robot'] * 10
 lst += ['human'] * 10
 random.shuffle(lst)
 data = pd.DataFrame({'whoAmI':lst})
-data.head()
 who = list(set([i for i in data['whoAmI']]))
 one_hot = data
 for im in who:
   one_hot[im] = 0
-  one_hot.loc[one_hot['whoAmI'] == im, im] = 1
+  one_hot.loc[one_hot[one_hot.columns[0]] == im, im] = 1
 one_hot.drop(one_hot.columns[[0]], axis=1, inplace=True)  
 print(one_hot) 
